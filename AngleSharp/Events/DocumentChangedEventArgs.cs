d6203530abe63cb694c0c1bfb30632a0f85f4d2a
Jonathan Close <jon.close@uti400.com>
2015-07-28 12:03:33 +0100

    Added missing file

---
+++ AngleSharp/Events/DocumentChangedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AngleSharp.Dom;
+
+namespace AngleSharp.Events
+{
+    public class DocumentChangedEventArgs : EventArgs
+    {
+        #region Properties
+
+        public IDocument NewDocument { get; }
+
+        #endregion
+
+        #region Methods
+
+        public DocumentChangedEventArgs(IDocument newDocument)
+        {
+            NewDocument = newDocument;
+        }
+
+        #endregion
+    }
+}
